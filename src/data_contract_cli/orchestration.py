99e07570fe254b29abe19c08177fe724e925409c
Alex <alexis.kuypers.ai@gmail.com>
2026-09-17 19:04:08 +0200

    feat: add CSV uniqueness validation

---
+++ src/data_contract_cli/orchestration.py
@@ -0,0 +1,26 @@
+from data_contract_cli.contract_models import Contract, Columns_Contract
+from data_contract_cli.contract import load_and_validate_contract
+from data_contract_cli.validate_csv import load_and_validate_csv
+from data_contract_cli.row_processing import validate_and_transform_csv_values
+from data_contract_cli.validate_unique import validate_unique
+from pathlib import Path
+
+
+def orchestration(yaml_contract_path: Path, csv_path: Path, mode: str):
+    yaml_contract = load_and_validate_contract(yaml_contract_path)
+    classified_rows = load_and_validate_csv(path=csv_path, contract=yaml_contract)
+    cleaned_classified_rows = validate_and_transform_csv_values(
+        classified_rows=classified_rows, contract=yaml_contract, mode=mode
+    )
+    validated_unique = validate_unique(
+        contrat=yaml_contract,
+        cleaned_classified_rows=cleaned_classified_rows,
+        mode=mode,
+    )
+
+    return classified_rows
+
+
+csv = Path("examples") / "example-4.csv"
+yaml_contract = Path("examples") / "valid-contract.yaml"
+print(orchestration(yaml_contract_path=yaml_contract, csv_path=csv, mode="permissive"))
